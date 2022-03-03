9e2b67f16e41da9761dd00ca7d4ff034a7dbe50d
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2022-03-03 10:14:29 +0000

    Changed path in Program.cs

--- sample_project/Program/Program.cs
+++ sample_project/Program/Program.cs
@@ -6,7 +6,7 @@ namespace University
     public class Program
     {
         public static void Main(String[] args) {
-            String path = "University.Tests/bin/Debug/net6.0/input.txt";
+            String path = "sample_project/University.Tests/bin/Debug/net6.0/input.txt";
             using (StreamWriter sw = File.CreateText(path))
             {
                 sw.WriteLine("3");
